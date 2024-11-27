b53e63d70a3b3f4024516d9989c81931c43c193b
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-27 15:31:33 +0600

    added titleBarOverlay- false, titleBarStyle - hidden

--- electron/main.ts
+++ electron/main.ts
@@ -28,11 +28,13 @@ let win: BrowserWindow | null
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    titleBarOverlay: false,
+    titleBarStyle: "hidden",
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
     webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
+      preload: path.join(__dirname, "preload.mjs"),
     },
-  })
+  });
 
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
